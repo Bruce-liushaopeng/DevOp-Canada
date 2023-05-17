ef94533ec9b6cc7b6feba1e6de8d5c971c671ff4
Your Name <you@example.com>
2023-05-16 21:49:01 -0400

    feat:add line to contact pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 function Contact() {
   return (
+    <>
     <h1>
       Contact Page
     </h1>
+    <p>Please contact devops team for any queries</p>
+    </>
   );
 }
 
