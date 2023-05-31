9e1b41363d8389e4297cac88c8a1d4857e27e9a2
jadeleafmoon <jadeleafmoon@gmail.com>
2023-05-30 21:40:44 -0400

    feat: add content to Homepage

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Blog() {
+  return (
+    <h1>This is our Blog Page. Welcome!</h1>
+  );
+}
+
+export default Blog;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <h1>Home Page</h1>
-  )
+    <div>
+      <h1>Home Page</h1>
+      <p>Picture</p>
+      <p>Welcome to DevOps Canada!</p>
+      <p>What is DevOps? Well, you came to the right place to learn!</p>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
