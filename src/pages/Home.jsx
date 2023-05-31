a6d12b848cef21c5409b7e61747b5040a4cbf86a
jadeleafmoon <jadeleafmoon@gmail.com>
2023-05-30 21:55:58 -0400

    feat: center h1 tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,9 @@ import React from "react";
 function Home() {
   return (
     <div>
-      {/* <img src="./img/banner.jpg"/> */}
       <img src="./img/banner.jpg" style={{ width: "100%", height: "auto" }} />
 
-      <h1>Welcome to DevOps Canada!</h1>
+      <h1 style={{ textAlign: "center" }}>Welcome to DevOps Canada!</h1>
       <p>What is DevOps? Well, you came to the right place to learn!</p>
     </div>
   );
