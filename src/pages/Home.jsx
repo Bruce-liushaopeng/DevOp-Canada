09b1d25f3dc1a224661b1a606c06c0b971dd9285
jadeleafmoon <jadeleafmoon@gmail.com>
2023-05-30 22:04:19 -0400

    feat: set image to 50% height

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <img src="./img/banner.jpg" style={{ width: "100%", height: "auto" }} />
+      <div style={{ height: "40vh", overflow: "hidden" }}>
+        <img src="./img/banner.jpg" style={{ width: "100%", height: "auto" }} />
+      </div>
 
       <h1 style={{ textAlign: "center" }}>Welcome to DevOps Canada!</h1>
       <p>What is DevOps? Well, you came to the right place to learn!</p>
