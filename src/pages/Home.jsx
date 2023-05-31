a2798d309ad7201aa9fde4c47761eb7315114d8b
jadeleafmoon <jadeleafmoon@gmail.com>
2023-05-30 21:51:25 -0400

    feat: add banner img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,10 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Picture</p>
-      <p>Welcome to DevOps Canada!</p>
+      {/* <img src="./img/banner.jpg"/> */}
+      <img src="./img/banner.jpg" style={{ width: "100%", height: "auto" }} />
+
+      <h1>Welcome to DevOps Canada!</h1>
       <p>What is DevOps? Well, you came to the right place to learn!</p>
     </div>
   );
